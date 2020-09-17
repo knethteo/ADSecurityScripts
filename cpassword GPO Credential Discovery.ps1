79de9cbf5293cee3738e62049e0abc71d58b1926
Kenneth Teo <kennethteo@msn.com>
2020-09-17 14:37:36 +0800

    Update cpassword GPO Credential Discovery.ps1

--- cpassword GPO Credential Discovery.ps1
+++ cpassword GPO Credential Discovery.ps1
@@ -1,1 +1,3 @@
+#Find potential password in cpassword read more here: https://adsecurity.org/?p=2288
+#Negative result doesn't mean passwords do not exist in GPO files, it may be in other forms.
 findstr /S /I cpassword \\<FQDN>\sysvol\<FQDN>\policies\*.xml
