e613c7392e303075126a318fa74f425556653f3f
Kenneth Teo <kennethteo@msn.com>
2021-01-29 12:10:20 +0800

    Update DiscoverDelegation.ps1

--- DiscoverDelegation.ps1
+++ DiscoverDelegation.ps1
@@ -3,4 +3,4 @@
 #https://adsecurity.org/?p=1667
 Import-Module ActiveDirectory
 
-Get-ADComputer -Filter {(TrustedForDelegation -eq $True) -AND (PrimaryGroupID -eq 515)} -Properties TrustedForDelegation,TrustedToAuthForDelegation,servicePrincipalName,Description | Export-Csv -Path .\WmiData.csv -NoTypeInformation
+Get-ADComputer -Filter {(TrustedForDelegation -eq $True) -AND (PrimaryGroupID -eq 515)} -Properties TrustedForDelegation,TrustedToAuthForDelegation,servicePrincipalName,Description | Export-Csv -Path .\DiscDelegation.csv -NoTypeInformation
