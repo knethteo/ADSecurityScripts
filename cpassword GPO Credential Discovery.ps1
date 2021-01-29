48ca9806dac92da7b679dba1e5b38c0da829c4cd
Kenneth Teo <kennethteo@msn.com>
2021-01-29 12:15:23 +0800

    Add export and multiple strings

--- cpassword GPO Credential Discovery.ps1
+++ cpassword GPO Credential Discovery.ps1
@@ -1,3 +1,3 @@
 #Find potential password in cpassword read more here: https://adsecurity.org/?p=2288
 #Negative result doesn't mean passwords do not exist in GPO files, it may be in other forms.
-findstr /S /I cpassword \\<FQDN>\sysvol\<FQDN>\policies\*.xml
+findstr /S /I /c:"cpassword" /c:"password" \\<FQDN>\sysvol\<FQDN>\policies\*.xml | Export-Csv -Path .\GPOPWD.csv -NoTypeInformation
