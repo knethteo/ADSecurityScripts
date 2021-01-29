df259357c353b0aa1b59f938c803433c663ff119
Kenneth Teo <kennethteo@msn.com>
2021-01-29 12:10:54 +0800

    Update to include export

--- PrivilegedAccountsRunningasKerberosServices.ps1
+++ PrivilegedAccountsRunningasKerberosServices.ps1
@@ -1,3 +1,3 @@
 ﻿#Get POTENTIALLY privileged users with SPN - high risk of kerberoasting
 #POTENTIALLY because AdminCount is not a reliable way to check for privileged accounts
-get-aduser -filter {(AdminCount -eq 1) -AND (ServicePrincipalName -notlike "*")} -Properties Name,AdminCount,ServicePrincipalName,PasswordLastSet,LastLogonDate,MemberOf
+get-aduser -filter {(AdminCount -eq 1) -AND (ServicePrincipalName -notlike "*")} -Properties Name,AdminCount,ServicePrincipalName,PasswordLastSet,LastLogonDate,MemberOf | Export-Csv -Path .\WmiData.csv -NoTypeInformation
